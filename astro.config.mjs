7ea883fc091ecfa30c5dd375ea4423c958a6b6ac
Christian Drerup <49913673+ChrisDrerup@users.noreply.github.com>
2026-08-13 00:40:25 +0000

    Move site to chrisdrerup.github.io

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,9 +5,5 @@ import tailwind from '@astrojs/tailwind';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://chrisdrerup.github.io',
-  base:
-    process.env.NODE_ENV === 'production'
-      ? '/christiandrerup.github.io'
-      : '',
   integrations: [mdx(), tailwind()]
 });
